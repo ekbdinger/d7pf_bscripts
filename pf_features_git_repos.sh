842a60aaacda53f21b8aed115e79610dc7c5c1c9
Paul <paulbosox@yahoo.com>
2012-10-03 20:24:27 -0400

    first commit

---
+++ pf_features_git_repos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# these are all of the git repos I have on the pressflow site as of October 3, 2012
+
+cd /Users/paul/sites/pressflow/sites/all/modules/feats
+
+git clone git://github.com/ekbdinger/d7pf_panel_pp_links.git
+git clone git://github.com/ekbdinger/d7pf_pp_all_views.git
+git clone git://github.com/ekbdinger/d7pf_pp_cck_descriptions.git
+git clone git://github.com/ekbdinger/d7pf_pp_links_right.git
+git clone git://github.com/ekbdinger/d7pf_pp_panel.git
+git clone git://github.com/ekbdinger/d7pf_pp_style_tests.git
+git clone git://github.com/ekbdinger/d7pf_pp_v1.git
