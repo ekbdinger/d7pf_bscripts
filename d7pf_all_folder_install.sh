d9bd2647870d79aa187abcd76d51671af1b2abb8
Paul <paulbosox@yahoo.com>
2012-10-03 20:34:41 -0400

    adding file to do a git pull of all folder into the right location

---
+++ d7pf_all_folder_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this script will install the all directory from github in the correct spot
+# just a placeholder for right now, will probably combine this with more 
+# commands to write a longer script
+# 10/03/2012
+
+cd /Users/paul/sites/pressflow/sites
+
+git clone git://github.com/ekbdinger/d7pf7_all_folder.git
